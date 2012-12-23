f54c957cd794a52dc7a898212c73f9a930c2d7b6
Thunderofnl <tunderofnl@gmail.com>
2012-12-23 18:47:37 +0100

    Converted to .Net 2.0 all the Windows OS's XP or higher should work.

--- Config Source/SteamTV_Config/SteamTV_Config/Form1.cs
+++ Config Source/SteamTV_Config/SteamTV_Config/Form1.cs
@@ -18,10 +18,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace SteamTV_Config
 {
